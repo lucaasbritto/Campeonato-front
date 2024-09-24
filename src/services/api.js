21f368f6ca09dbeba9389124b43e35e980edf1b3
Lucas Britto <mister.britto@hotmail.com>
2024-09-24 19:19:08 -0300

    1009 - Configuracao de rota para ambiente de teste e producao

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:8000/api',
+  baseURL: process.env.VUE_APP_API_BASE_URL ,
   headers: {
     'Content-Type': 'application/json',
   },
